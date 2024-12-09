cdaa436bd7c2d4bd180443214d2b717967839e16
Orson <2736043@collegelacite.ca>
2024-12-09 18:07:05 -0500

    première version du code

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Hello, World!");
+var a = 10;
+var b = 23;
+var total = a +b;
+Console.WriteLine("Total = " + total);
