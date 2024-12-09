237965726b0b38d7414279e98f4c2957f333e2ad
Orson <2736043@collegelacite.ca>
2024-12-09 18:08:07 -0500

    deuxème version du code

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Bob!");
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Dan!");
 var a = 10;
 var b = 23;
 var total = a +b;
-Console.WriteLine("Total = " + total);
+Console.WriteLine("Total  = " + total);
